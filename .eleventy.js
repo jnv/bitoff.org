e3ddcb0f127a98dca7643aabd74d4cf60d7e81eb
Jan Vlnas <git.noreply@jan.vlnas.cz>
2022-07-23 22:34:40 +0200

    Remove header links (for now)

--- .eleventy.js
+++ .eleventy.js
@@ -20,11 +20,6 @@ module.exports = function (eleventyConfig) {
 
   eleventyConfig.amendLibrary("md", (mdLib) => {
     mdLib.use(markdownItAnchor, {
-      permalink: markdownItAnchor.permalink.ariaHidden({
-        placement: "after",
-        class: "direct-link",
-        symbol: "#",
-      }),
       level: [1, 2, 3, 4],
       slugify: eleventyConfig.getFilter("slug"),
     });
