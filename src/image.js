6c69f3ced3ed820f49d73e8474fc59e8b40a2275
Jan Vlnas <git.noreply@jan.vlnas.cz>
2022-08-11 19:57:51 +0200

    Calculate sizes like a baws

--- src/image.js
+++ src/image.js
@@ -3,10 +3,6 @@ const { escape } = require("html-escaper");
 const Image = require("@11ty/eleventy-img");
 
 const WIDTHS = [720, null];
-const SIZES = [
-  "(max-width: 42em) calc(100vw - 2rem)", // small screen with body padding
-  "calc(70ch - 80px)", // other viewports with figure's default margin
-];
 
 function getExt(src) {
   const ext = path.extname(src).substring(1);
@@ -34,6 +30,14 @@ function lastItem(arr) {
   return arr[arr.length - 1];
 }
 
+function getSizes(largestWidth) {
+  const sizes = [
+    "(max-width: 42em) calc(100vw - 2rem)", // small screen with body padding
+    `min(calc(70ch - 80px), ${largestWidth}px)`, // other viewports with figure's default margin - or the image width
+  ];
+  return sizes.join(",");
+}
+
 function imageShortcode(src, alt = "") {
   const srcExt = getExt(src);
   const options = {
@@ -49,22 +53,23 @@ function imageShortcode(src, alt = "") {
   // generate images, while this is async we don’t wait
   Image(fileSrc, options);
 
+  // get metadata even the images are not fully generated
+  const metadata = Image.statsSync(fileSrc, options);
+
+  const originalFile = lastItem(metadata[srcExt]);
+  const originalWidth = originalFile.width;
   const imageAttributes = {
     // ffs https://github.com/11ty/eleventy-img/issues/82
     alt: escape(alt),
     loading: "lazy",
     decoding: "async",
-    sizes: SIZES,
+    sizes: getSizes(originalWidth),
   };
-  // get metadata even the images are not fully generated
-  const metadata = Image.statsSync(fileSrc, options);
-
   const imageHtml = Image.generateHTML(metadata, imageAttributes, {
     whitespaceMode: "inline",
   });
 
-  const originalFile = lastItem(metadata[srcExt]);
-  if (originalFile.width > 300) {
+  if (originalWidth > 300) {
     return `<a href="${originalFile.url}" class="link-img">${imageHtml}</a>`;
   } else {
     return imageHtml;
