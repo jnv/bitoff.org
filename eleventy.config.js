b7aa262537707fa6c2483a59b126a5219ed56ed1
Jan Vlnas <git.noreply@jan.vlnas.cz>
2024-08-01 22:58:40 +0200

    Enable upgrade helper

--- eleventy.config.js
+++ eleventy.config.js
@@ -1,3 +1,4 @@
+const UpgradeHelper = require("@11ty/eleventy-upgrade-help");
 const pluginRss = require("@11ty/eleventy-plugin-rss");
 const pluginSyntaxHighlight = require("@11ty/eleventy-plugin-syntaxhighlight");
 const pluginNavigation = require("@11ty/eleventy-navigation");
@@ -45,6 +46,8 @@ module.exports = function (eleventyConfig) {
   eleventyConfig.setServerOptions({
     showVersion: true,
   });
+
+  eleventyConfig.addPlugin(UpgradeHelper);
 
   return {
     // Control which files Eleventy will process
