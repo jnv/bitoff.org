2857aa13a39e2585f0b9eb14955644d7d848fcdd
Jan Vlnas <git.noreply@jan.vlnas.cz>
2024-08-21 23:27:55 +0200

    Footnotes heading, comments separation

--- src/markdown.js
+++ src/markdown.js
@@ -1,10 +1,14 @@
-import markdownItAnchor from "markdown-it-anchor";
-import footnote from "markdown-it-footnote";
+import markdownItAnchor from 'markdown-it-anchor';
+import footnote from 'markdown-it-footnote';
 
 export default (mdLib, eleventyConfig) => {
   mdLib.use(markdownItAnchor, {
     level: [1, 2, 3, 4],
-    slugify: eleventyConfig.getFilter("slug"),
+    slugify: eleventyConfig.getFilter('slug'),
   });
   mdLib.use(footnote);
+  mdLib.renderer.rules.footnote_block_open = () =>
+    '<section class="footnotes">\n' +
+    '<h2>Footnotes</h2>\n' +
+    '<ol class="footnotes-list">\n';
 };
