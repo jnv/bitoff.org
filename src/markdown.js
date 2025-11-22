b755e266e930a70a1cfc4f5e70559165ae49f052
Jan Vlnas <git.noreply@jan.vlnas.cz>
2025-11-22 13:38:10 +0100

    Improve anchors handling, slugify

--- src/markdown.js
+++ src/markdown.js
@@ -1,27 +1,40 @@
 import markdownItAnchor from 'markdown-it-anchor';
 import footnote from 'markdown-it-footnote';
 import markdownItReplaceLink from 'markdown-it-replace-link';
-import path from 'node:path';
 
 export default (mdLib, eleventyConfig) => {
+  const slugify = eleventyConfig.getFilter('slugify');
   mdLib.use(markdownItAnchor, {
     level: [1, 2, 3, 4],
-    slugify: eleventyConfig.getFilter('slug'),
+    slugify,
   });
   mdLib.use(footnote);
   mdLib.use(markdownItReplaceLink, {
     processHTML: true,
-    replaceLink: (link, env, token, htmlToken) => {
-      if (!link.match(/^\.\.?\//)) {
+    replaceLink: (link) => {
+      if (!/^\.\.?\/.+\.md/.test(link)) {
         return link;
       }
-      // console.log('Replacing link:', link);
-      let cleanLink = link.replace(/\/index\.md$/, '').replace(/\.md$/, '');
-      const basename = path.basename(cleanLink);
-      const withoutDate = basename.replace(/^\d{4}-\d{2}-\d{2}-/, '');
 
-      // console.log('Cleaned link:', '/' + withoutDate);
-      return '/' + withoutDate;
+      try {
+        const u = new URL(link, 'http://n/');
+
+        const segments = u.pathname.split('/').filter(Boolean);
+        if (!segments.length) {
+          return link;
+        }
+        let last = segments.pop();
+        if (last === 'index.md') {
+          last = segments.pop() || last;
+        }
+        const finalPath = last
+          .replace(/.md$/, '')
+          .replace(/^\d{4}-\d{2}-\d{2}-/, '');
+        // console.log(`Rewriting link ${link} to /${finalPath}${u.hash}`);
+        return '/' + finalPath + u.hash;
+      } catch {
+        return link;
+      }
     },
   });
   mdLib.renderer.rules.footnote_block_open = () =>
